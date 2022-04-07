8dd774d59b288690770c82ad8f947c7941f932fb
utku <m.utku.k@gmail.com>
2022-04-07 03:57:54 +0300

    init with hazelcast 5.1.1

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hz-live-sim
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hz_live_sim/core_test.clj
@@ -0,0 +1,7 @@
+(ns hz-live-sim.core-test
+  (:require [clojure.test :refer :all]
+            [hz-live-sim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
